cc9249d8c5f5ab35ad53575b9232be5b72eb6457
Cyberis <christopher.bortz@gmail.com>
2025-12-25 00:53:12 -0500

    Add Function To Convert Inline Elements to LeafNodes

---
+++ src/nodes.py
@@ -0,0 +1,19 @@
+from textnode import TextNode, TextType
+from htmlnode import HTMLNode, LeafNode, ParentNode
+
+def text_node_to_html_node(text_node):
+    match text_node.text_type:
+        case TextType.TEXT:
+            return LeafNode(None, value=text_node.text)
+        case TextType.BOLD:
+            return LeafNode("b", value=text_node.text)
+        case TextType.ITALIC:
+            return LeafNode("i", value=text_node.text)
+        case TextType.CODE:
+            return LeafNode("code", value=text_node.text)
+        case TextType.LINK:
+            return LeafNode("a", value=text_node.text, props={"href": text_node.url})
+        case TextType.IMAGE:
+            return LeafNode("img", "", props={"src": text_node.url, "alt": text_node.text})
+        case _:
+            raise ValueError(f"Unsupported TextType: {text_node.text_type}")
---
+++ src/test_nodes.py
@@ -0,0 +1,49 @@
+import unittest
+from htmlnode import HTMLNode, LeafNode, ParentNode
+from textnode import TextNode, TextType
+from nodes import text_node_to_html_node
+
+class TestNodes(unittest.TestCase):
+    def test_text_node_to_html_node_text(self):
+        text_node = TextNode("Just some text", TextType.TEXT)
+        html_node = text_node_to_html_node(text_node)
+        expected_node = LeafNode(None, "Just some text")
+        self.assertEqual(html_node, expected_node)
+        
+    def test_text_node_to_html_node_bold(self):
+        text_node = TextNode("Bold text", TextType.BOLD)
+        html_node = text_node_to_html_node(text_node)
+        expected_node = LeafNode("b", "Bold text")
+        self.assertEqual(html_node, expected_node)
+        
+    def test_text_node_to_html_node_italic(self):
+        text_node = TextNode("Italic text", TextType.ITALIC)
+        html_node = text_node_to_html_node(text_node)
+        expected_node = LeafNode("i", "Italic text")
+        self.assertEqual(html_node, expected_node)
+        
+    def test_text_node_to_html_node_code(self):
+        text_node = TextNode("Code snippet", TextType.CODE)
+        html_node = text_node_to_html_node(text_node)
+        expected_node = LeafNode("code", "Code snippet")
+        self.assertEqual(html_node, expected_node)
+        
+    def test_text_node_to_html_node_link(self):
+        text_node = TextNode("Click here", TextType.LINK, "https://example.com")
+        html_node = text_node_to_html_node(text_node)
+        expected_node = LeafNode("a", "Click here", props={"href": "https://example.com"})
+        self.assertEqual(html_node, expected_node)
+        
+    def test_text_node_to_html_node_image(self):
+        text_node = TextNode("An image", TextType.IMAGE, "https://example.com/image.png")
+        html_node = text_node_to_html_node(text_node)
+        expected_node = LeafNode("img", "", props={"src": "https://example.com/image.png", "alt": "An image"})
+        self.assertEqual(html_node, expected_node)
+        
+    def test_text_node_to_html_node_unsupported_type(self):
+        class FakeTextType:
+            UNKNOWN = 99
+            
+        text_node = TextNode("Unknown type", FakeTextType.UNKNOWN)
+        with self.assertRaises(ValueError):
+            text_node_to_html_node(text_node)
